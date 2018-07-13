13a4e076df9afdb31519646349c9176c21bdc887
joanna-gabis <joanna.gabis@gmail.com>
2018-07-13 16:50:59 +0200

    added comment function - add/delete photo comments

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,7 +1,31 @@
 // a reducer takes in two things: the action (info about what happened) and copy of current state
 
+function postComments(state=[], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      // return the new state with a new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment,
+      }];
+    case 'REMOVE_COMMENT':
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1),
+      ];
+    default:
+      return state
+  }
+  return state;
+}
+
 function comments(state=[], action) {
-  
+  if(typeof action.postId !== 'undefined') {
+    return {
+      ...state,
+      [action.postId]: postComments(state[action.postId], action),
+    }
+  }
   return state;
 }
 
